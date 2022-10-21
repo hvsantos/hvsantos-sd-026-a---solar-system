6735197a275fb4d4437b3d5060c7c9b2a302ec6d
Henrique Vilela <hv_santos@outlook.com>
2022-10-21 14:50:16 -0300

    Req 4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Title from './components/Title';
 
 class App extends React.Component {
   render() {
-    const headline = 'Ganhamo';
     return (
       <div>
         <Header />
         <p>Sistema Solar</p>
         <SolarSystem />
-        <Title headline={ headline } />
       </div>
     );
   }
--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Title from './Title';
 
 export default class SolarSystem extends Component {
   render() {
+    const headline = 'Planetas';
     return (
-      <div data-testid="solar-system"> </div>
+      <div data-testid="solar-system">
+        <Title headline={ headline } />
+      </div>
     );
   }
 }
