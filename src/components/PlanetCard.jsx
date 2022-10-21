dfb0623005f7bb130fe91a3d5e316d913802b857
Henrique Vilela <hv_santos@outlook.com>
2022-10-21 14:57:03 -0300

    req 5

---
+++ src/components/PlanetCard.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class PlanetCard extends Component {
+  render() {
+    const { planetName, planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <p data-testid="planet-name">{ planetName }</p>
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetImage: PropTypes.string,
+  planetName: PropTypes.string,
+}.isRequired;
