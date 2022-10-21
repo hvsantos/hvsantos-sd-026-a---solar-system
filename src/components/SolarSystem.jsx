77e656827bfdd77d33dc3a591bb169509bfaf1e9
Henrique Vilela <hv_santos@outlook.com>
2022-10-21 15:04:40 -0300

    req 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
+import Planets from '../data/planets';
 
 export default class SolarSystem extends Component {
   render() {
@@ -7,6 +9,11 @@ export default class SolarSystem extends Component {
     return (
       <div data-testid="solar-system">
         <Title headline={ headline } />
+        <div>
+          { Planets.map(({ name, image }, index) => (
+            <PlanetCard key={ index } planetName={ name } planetImage={ image } />
+          )) }
+        </div>
       </div>
     );
   }
